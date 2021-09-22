d0321a1c816aefc97de253eaebd4f24086f10554
travhoanng77 <travhoang77>
2021-09-22 13:39:19 -0700

    Partial Landing page

--- src/component/MainCarousel.js
+++ src/component/MainCarousel.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import msi from "../assets/msi-image.jpg";
-import nvda from "../assets/nvd-image.jpg";
-import asus from "../assets/asus-image.JPG";
 import { CAROUSEL_TIME } from "../utils/constants";
 
 function MainCarousel(props) {
+  const msi = "msi-image.jpg";
+  const nvda = "nvd-image.jpg";
+  const asus = "asus-image.JPG";
   return (
     <Carousel
       style={{
@@ -13,14 +13,18 @@ function MainCarousel(props) {
       }}
     >
       <Carousel.Item interval={CAROUSEL_TIME}>
-        <img className="d-block w-100" src={msi} alt="First slide" />
+        <img className="d-block w-100" src={`/img/${msi}`} alt="First slide" />
         <Carousel.Caption>
           <h3>MSI Exclusive Ampere</h3>
           <p>Ray Tracying Enabled</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={CAROUSEL_TIME}>
-        <img className="d-block w-100" src={asus} alt="Second slide" />
+        <img
+          className="d-block w-100"
+          src={`/img/${nvda}`}
+          alt="Second slide"
+        />
         <Carousel.Caption>
           <h3>ASUS 30 Series</h3>
         </Carousel.Caption>
@@ -28,7 +32,7 @@ function MainCarousel(props) {
       <Carousel.Item interval={CAROUSEL_TIME}>
         <img
           className="d-block w-100"
-          src={nvda}
+          src={`/img/${asus}`}
           alt="Nvidia Founders Edition"
         />
         <Carousel.Caption></Carousel.Caption>
